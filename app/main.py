84ef4042cefba0fba9907de41cc783ce930a7324
岩本圭介 <mcpu3@outlook.jp>
2022-09-19 02:25:30 +0900

    ルーティングを修正 #2

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from api.v1.mock import mock
 
 
 app = FastAPI()
-app.include_router(mock.api_router, prefix='/api/v1')
+app.include_router(mock.api_router, prefix='/api/v1/mock')
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0")
