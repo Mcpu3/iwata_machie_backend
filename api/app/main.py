4aa591cbfe5460ade346963e08f79f646e80cbf1
岩本圭介 <mcpu3@outlook.jp>
2022-09-22 03:16:33 +0900

    CORSを追加 #9

--- api/app/main.py
+++ api/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from api.v1 import v1
@@ -6,6 +7,12 @@ from api.v1.mock import mock
 
 
 app = FastAPI()
+app.add_middleware(CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 app.include_router(v1.api_router, prefix='/api/v1')
 app.include_router(mock.api_router, prefix='/api/v1/mock')
 
